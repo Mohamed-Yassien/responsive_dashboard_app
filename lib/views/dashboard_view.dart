353db1e4d43f4469ba908e45af3fd7d665780c5b
mali <mali@tafeel.com>
2024-07-08 11:07:58 +0300

    init app with base files

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashboard_app/utils/size_config.dart';
+
+import '../widgets/adaptive_layout_widget.dart';
+
+class DashBoradView extends StatefulWidget {
+  const DashBoradView({super.key});
+
+  @override
+  State<DashBoradView> createState() => _DashBoradViewState();
+}
+
+class _DashBoradViewState extends State<DashBoradView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig.init(context);
+    return Scaffold(
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+          ? AppBar(
+              elevation: 0,
+              backgroundColor: const Color(0xFFFAFAFA),
+              leading: IconButton(
+                  onPressed: () {
+                    scaffoldKey.currentState!.openDrawer();
+                  },
+                  icon: const Icon(Icons.menu)),
+            )
+          : null,
+      backgroundColor: const Color(0xFFF7F9FA),
+      // drawer: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+      //     ? const CustomDrawer()
+      //     : null,
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const SizedBox(),
+      ),
+    );
+  }
+}
